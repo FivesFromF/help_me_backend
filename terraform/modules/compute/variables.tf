a96e7074d695b1d9a17375beed87d8641abb161d
Mai Nguyen Duy Khanh <mndkhanh@gmail.com>
2026-04-18 01:25:24 +0700

    refine: switch terraform to other solutions

--- terraform/modules/compute/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "environment" {}
-variable "vpc_id" {}
-variable "public_subnets" {
-  type = list(string)
-}
-variable "private_subnets" {
-  type = list(string)
-}
-variable "alb_sg_id" {}
--- terraform/modules/database/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "environment" {}
-variable "vpc_id" {}
-variable "private_subnets" {
-  type = list(string)
-}
-variable "db_name" {}
-variable "master_username" {}
